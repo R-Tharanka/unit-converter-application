579b46bb4fd41a14144a9d080690fda4e6059b0a
Ruchira Tharanka <ruchiratharanka1@gmail.com>
2026-01-16 18:19:06 +0000

    refactor: add userId to History schema

--- backend/src/models/History.js
+++ backend/src/models/History.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const historySchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, index: true },
   value: { type: Number, required: true },
   from: { type: String, required: true },
   to: { type: String, required: true },
